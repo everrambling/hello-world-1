cf77a2076b19a0f2cf7c277464190aa2ca17ac13
nic-instruction <nicole.bade@seattlecolleges.edu>
2018-02-01 17:53:21 -0800

    Update apacheinstall.sh

--- apacheinstall.sh
+++ apacheinstall.sh
@@ -11,3 +11,25 @@ Welcome NTI-300
 </html>
 EOF
 
+cat <<EOF > /var/cron/crontab/root
+
+# ┌───────────── minute (0 - 59)
+# │ ┌───────────── hour (0 - 23)
+# │ │ ┌───────────── day of month (1 - 31)
+# │ │ │ ┌───────────── month (1 - 12)
+# │ │ │ │ ┌───────────── day of week (0 - 6) (Sunday to Saturday;
+# │ │ │ │ │                                       7 is also Sunday on some systems)
+# │ │ │ │ │
+# │ │ │ │ │
+# * * * * *  command to execute
+18,0 * * * * /usr/bin/echo "hello world"
+30 * * * * /home/nicolebade/hello-nti-300/automated_webpage.sh
+EOF
+
+
+
+cat <<EOF > /home/nicolebade/hello-nti-300/automated_webpage.sh
+#!/bin/bash
+echo -e "<html> \n <h1> Welcome, NTI-300 </h1> \n<h2> Yay, I'm having so much fun with linux</h2> \n <p> this is the date right now: $(date +c)</p> \n</html>" > /var/www/html/index.html
+EOF
+
